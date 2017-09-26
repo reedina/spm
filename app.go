a55a0c323509e3491079e24d1011e2130ee45668
Reedina <reedinafarms@gmail.com>
2017-09-26 10:09:45 -0700

    added cors support in app.go

--- app.go
+++ app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/reedina/spm/ctrl"
 	"github.com/reedina/spm/model"
+	"github.com/rs/cors"
 
 	//Initialize pq driver
 	_ "github.com/lib/pq"
@@ -58,5 +59,10 @@ func (a *App) InitializeRoutes() {
 
 //RunApplication - Start the HTTP server
 func (a *App) RunApplication(addr string) {
-	log.Fatal(http.ListenAndServe(addr, a.Router))
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+	})
+
+	log.Fatal(http.ListenAndServe(addr, c.Handler(a.Router)))
 }
